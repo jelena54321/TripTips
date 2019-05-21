63d44690305292e003e0677ff89d5ffa8ac6429d
five <fivefive@fives-MacBook-Pro-6.local>
2019-05-21 15:03:18 +0200

    Initial Commit

---
+++ iosProjekt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosProjekt
+//
+//  Created by five on 21/05/2019.
+//  Copyright © 2019 five. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
